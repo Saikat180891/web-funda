dfac700417a4219dbcca27f3e426ba8b5b11a6d4
DESKTOP-TB44FP1\Saikat <saikat.paul@bigbasket.com>
2021-12-14 21:09:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIOVZb1ymZ6Gs0ec-ehS2Ao4FGWuBuHJ8",
+  authDomain: "web-funda.firebaseapp.com",
+  projectId: "web-funda",
+  storageBucket: "web-funda.appspot.com",
+  messagingSenderId: "720789051631",
+  appId: "1:720789051631:web:b244cc07eb234ec7ede31a",
+  measurementId: "G-LQVWWZ502L",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
